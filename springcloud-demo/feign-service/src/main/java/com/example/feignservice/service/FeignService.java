5c03494cc576d2ce25015ce14ccd3d6598f424df
hanlei738 <54488915+hanlei738@users.noreply.github.com>
2020-06-08 09:36:20 +0800

    Add files via upload

---
+++ springcloud-demo/feign-service/src/main/java/com/example/feignservice/service/FeignService.java
@@ -0,0 +1,15 @@
+package com.example.feignservice.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(value = "client")
+@Component
+public interface FeignService {
+
+    @RequestMapping(value = "/hi", method = RequestMethod.GET)
+    String sayHiFromClientOne(@RequestParam(value = "name") String name);
+}
